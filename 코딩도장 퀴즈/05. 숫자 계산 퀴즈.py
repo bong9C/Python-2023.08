d82aba2a62ecee572556c077f1c96b473dd827bb
SeHee JU <kathy1221@naver.com>
2023-08-28 17:36:50 +0900

    2023.08.28 python 퀴즈 풀기

---
+++ 코딩도장 퀴즈/05. 숫자 계산 퀴즈.py
@@ -0,0 +1,10 @@
+print(7 + (10 -5) * 2)
+
+print(int(0.2467 * 12 + 4.159))
+
+print(102 * 0.6 +255)
+
+a,b,c,d = map(int,input().split())
+print((a+b+c+d)//4)
+
+print('Hello', '\n', 'Python', sep='')
